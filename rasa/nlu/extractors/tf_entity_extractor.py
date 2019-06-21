a71d573e59570fd69369c4ec2d8881f5dc7be341
裘锋锋 <qiufengfeng@skyworth.com>
2019-06-21 11:13:44 +0800

    增加测试

--- rasa/nlu/extractors/tf_entity_extractor.py
+++ rasa/nlu/extractors/tf_entity_extractor.py
@@ -36,6 +36,8 @@ from rasa.nlu.extractors.tf_utils.bert_data_utils import make_tfrecord_files as 
 from rasa.third_models.bert import modeling as bert_modeling
 import shutil
 from rasa.nlu.extractors.tf_models.params import Params,TestParams
+from sklearn.metrics import f1_score
+import tqdm
 
 
 
@@ -96,19 +98,65 @@ class TfEntityExtractor(EntityExtractor):
 
         os.environ["CUDA_VISIBLE_DEVICES"] = params.device_map
         with tf.Graph().as_default():
-            training_input_x, training_input_y = input_fn(os.path.join(params.output_path, 'train.tfrecord'),
-                                                          shuffle_num=params.shuffle_num,
-                                                          mode=tf.estimator.ModeKeys.TRAIN,
-                                                          batch_size=params.batch_size,
-                                                          max_sentence_length=params.max_sentence_length,
-                                                          )
-            if params.ner_type == "idcnn":
-                classify_model = IdCnn(params)
-            else:
-                classify_model = BiLSTM(params)
-            classify_model.train(training_input_x, training_input_y)
+            session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+            session_conf.gpu_options.allow_growth = True
+            session_conf.gpu_options.per_process_gpu_memory_fraction = 0.9
 
-            self.component_config['pb_path'] = classify_model.make_pb_file(params.output_path)
+            sess = tf.Session(config=session_conf)
+            with sess.as_default():
+                training_input_x, training_input_y = input_fn(os.path.join(params.output_path, 'train.tfrecord'),
+                                                              shuffle_num=params.shuffle_num,
+                                                              mode=tf.estimator.ModeKeys.TRAIN,
+                                                              batch_size=params.batch_size,
+                                                              max_sentence_length=params.max_sentence_length,
+                                                              )
+                if params.ner_type == "idcnn":
+                    ner_model = IdCnn(params)
+                else:
+                    ner_model = BiLSTM(params)
+                loss, global_step, train_op, merger_op = ner_model.make_train(training_input_x, training_input_y)
+                # 初始化所有变量
+                saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
+                ner_model.model_restore(sess, saver)
+
+                best_f1 = 0
+                for _ in tqdm.tqdm(range(params.total_train_steps), desc="steps", miniters=10):
+                    sess_loss, steps, _ = sess.run([loss, global_step, train_op])
+
+                    if steps % params.evaluate_every_steps == 1:
+                        test_input_x, test_input_y = input_fn(os.path.join(params.output_path, "test.tfrecord"),
+                                                              shuffle_num=params.shuffle_num,
+                                                              batch_size=params.batch_size,
+                                                              max_sentence_length=params.max_sentence_length,
+                                                              mode=tf.estimator.ModeKeys.EVAL)
+                        loss_test, predict_test, sentence_length = ner_model.make_test(test_input_x, test_input_y)
+
+                        predict_var = []
+                        train_y_var = []
+                        loss_total = 0
+                        num_batch = 0
+                        try:
+                            while 1:
+                                loss_, predict_, test_input_y_, length = sess.run(
+                                    [loss_test, predict_test, test_input_y, sentence_length])
+                                loss_total += loss_
+                                num_batch += 1
+                                for p_, t_, len_ in zip(predict_.tolist(), test_input_y_.tolist(), length.tolist()):
+                                    predict_var += p_[:len_]
+                                    train_y_var += t_[:len_]
+                        except tf.errors.OutOfRangeError:
+                            print("eval over")
+                        if num_batch > 0:
+
+                            f1_val = f1_score(train_y_var, predict_var, average='micro')
+                            print("current step:%s ,loss:%s , f1 :%s" % (steps, loss_total / num_batch, f1_val))
+
+                            if f1_val >= best_f1:
+                                saver.save(sess, params.model_path, steps)
+                                print("new best f1: %s ,save to dir:%s" % (f1_val, params.output_path))
+                                best_f1 = f1_val
+
+                self.component_config['pb_path'] = ner_model.make_pb_file(params.output_path)
 
     def bert_train(self,args):
         os.environ['CUDA_VISIBLE_DEVICES'] = args.device_map
--- rasa/nlu/extractors/tf_models/base_ner_model.py
+++ rasa/nlu/extractors/tf_models/base_ner_model.py
@@ -38,20 +38,21 @@ class BasicNerModel():
         return length
 
     def create_model(self,input_x,dropout,already_embedded=False,real_sentence_length=None):
-        if not already_embedded:
-            with tf.variable_scope('embeddings_layer'):
-                word_embeddings = tf.get_variable('word_embeddings', [self.params.vocab_size, self.params.embedding_size])
-                input_embeddings = tf.nn.embedding_lookup(word_embeddings, input_x)
-            real_sentence_length = self.get_setence_length(input_x)
-        else:
-            input_embeddings = input_x
-            real_sentence_length = real_sentence_length
+        with tf.variable_scope("model_define", reuse=tf.AUTO_REUSE) as scope:
+            if not already_embedded:
+                with tf.variable_scope('embeddings_layer'):
+                    word_embeddings = tf.get_variable('word_embeddings', [self.params.vocab_size, self.params.embedding_size])
+                    input_embeddings = tf.nn.embedding_lookup(word_embeddings, input_x)
+                real_sentence_length = self.get_setence_length(input_x)
+            else:
+                input_embeddings = input_x
+                real_sentence_length = real_sentence_length
 
-        model_output = self.model_layer(input_embeddings,dropout,real_sentence_length)
-        logits = self.project_layer(model_output)
+            model_output = self.model_layer(input_embeddings,dropout,real_sentence_length)
+            logits = self.project_layer(model_output)
 
-        trans = tf.get_variable('transitions',
-                                     shape=[self.params.num_tags,self.params.num_tags],)
+            trans = tf.get_variable('transitions',
+                                         shape=[self.params.num_tags,self.params.num_tags],)
         return logits,real_sentence_length,trans
 
     def model_layer(self, model_inputs, dropout,sequence_length=None):
@@ -75,59 +76,34 @@ class BasicNerModel():
 
 
 
-    def train(self,input_x,input_y):
-        session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
-        session_conf.gpu_options.allow_growth = True
-        session_conf.gpu_options.per_process_gpu_memory_fraction = 0.9
+    def make_train(self,input_x,input_y):
+        dropout = tf.placeholder_with_default(0.8,shape=[], name='dropout')
+        logits,real_sentence_length,trans = self.create_model(input_x, dropout)
+        global_step = tf.Variable(0, name="globalStep", trainable=False)
+        with tf.variable_scope('loss'):
+            loss = self.crf_layer_loss(logits,input_y,real_sentence_length,trans)
+            optimizer = tf.train.AdamOptimizer(self.params.learning_rate)
 
-        sess = tf.Session(config=session_conf)
-        with sess.as_default():
-            dropout = tf.placeholder(dtype=tf.float32, name='dropout')
-            logits,real_sentence_length,trans = self.create_model(input_x, dropout)
-            globalStep = tf.Variable(0, name="globalStep", trainable=False)
-            with tf.variable_scope('loss'):
-                loss = self.crf_layer_loss(logits,input_y,real_sentence_length,trans)
-                optimizer = tf.train.AdamOptimizer(self.params.learning_rate)
+            grads_and_vars = optimizer.compute_gradients(loss)
+            train_op = optimizer.apply_gradients(grads_and_vars, global_step)
 
-                grads_and_vars = optimizer.compute_gradients(loss)
-                trainOp = optimizer.apply_gradients(grads_and_vars, globalStep)
+        pred_ids, _ = crf.crf_decode(potentials=logits, transition_params=trans, sequence_length=real_sentence_length)
+        pred_ids = tf.identity(pred_ids,name=constant.OUTPUT_NODE_NAME)
+        with tf.variable_scope('summary'):
+            tf.summary.scalar("loss", loss)
+            summary_op = tf.summary.merge_all()
 
-            pred_ids, _ = crf.crf_decode(potentials=logits, transition_params=trans, sequence_length=real_sentence_length)
-            pred_ids = tf.identity(pred_ids,name=constant.OUTPUT_NODE_NAME)
-            with tf.variable_scope('summary'):
-                tf.summary.scalar("loss", loss)
-                summary_op = tf.summary.merge_all()
+        return loss,global_step,train_op,summary_op
 
-            # 初始化所有变量
-            saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
+    def make_test(self,input_x,input_y):
+        dropout = tf.placeholder_with_default(1.0,(), name='dropout')
 
-            sess.run(tf.global_variables_initializer())
-            steps = 0
-
-            tf_save_path = os.path.join(self.params.output_path, 'tf')
-            try:
-                best_f1 = 0
-                for _ in tqdm.tqdm(range(self.params.total_train_steps), desc="steps", miniters=10):
-                    sess_loss, predict_var, steps, _,real_sentence,input_y_val  = sess.run(
-                        [loss, pred_ids, globalStep, trainOp,real_sentence_length,input_y],
-                        feed_dict={dropout: 0.8}
-                    )
-
-                    if steps % self.params.evaluate_every_steps == 0:
-                        train_labels = []
-                        predict_labels = []
-                        for train_, predict_,len_ in zip(input_y_val, predict_var,real_sentence):
-                            train_labels += train_[:len_].tolist()
-                            predict_labels += predict_[:len_].tolist()
-                        f1_val = f1_score(train_labels, predict_labels, average='micro')
-                        print("current step:%s ,loss:%s ,f1 :%s" % (steps, sess_loss,f1_val))
-
-                        if f1_val > best_f1:
-                            saver.save(sess, tf_save_path, steps)
-                            print("new best f1: %s ,save to dir:%s" % (f1_val,self.params.output_path))
-                            best_f1 = f1_val
-            except tf.errors.OutOfRangeError:
-                print("training end")
+        logits, real_sentence_length, trans = self.create_model(input_x, dropout)
+        with tf.variable_scope('loss'):
+            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=input_y))
+        pred_ids, _ = crf.crf_decode(potentials=logits, transition_params=trans, sequence_length=real_sentence_length)
+        pred_ids = tf.identity(pred_ids, name=constant.OUTPUT_NODE_NAME)
+        return loss,pred_ids,real_sentence_length
 
     def make_pb_file(self,model_dir):
         graph = tf.Graph()
@@ -174,3 +150,19 @@ class BasicNerModel():
         input_node = sess.graph.get_operation_by_name(constant.INPUT_NODE_NAME).outputs[0]
         predict_node = sess.graph.get_operation_by_name(constant.OUTPUT_NODE_NAME).outputs[0]
         return sess, input_node, predict_node
+
+
+    def model_restore(self, sess, tf_saver):
+        '''
+        模型恢复或者初始化
+        :param sess: 
+        :param tf_saver: 
+        :return: 
+        '''
+        ckpt = tf.train.get_checkpoint_state(os.path.dirname(self.params.model_path))
+        if ckpt and ckpt.model_checkpoint_path:
+            print("restor model")
+            tf_saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            print("init model")
+            sess.run(tf.global_variables_initializer())
