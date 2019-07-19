85fea39816abcfcbc82b8db017c6bedc0ede695a
裘锋锋 <qiufengfeng@skyworth.com>
2019-07-19 14:33:23 +0800

    文件修改提交

--- rasa/nlu/classifiers/tf_models/classify_ensemble_model.py
+++ rasa/nlu/classifiers/tf_models/classify_ensemble_model.py
@@ -1,8 +1,7 @@
-from category.tf_models.base_classify_model import BaseClassifyModel
-from category.tf_models.classify_cnn_model import ClassifyCnnModel
-from category.tf_models.classify_bilstm_model import ClassifyBilstmModel
-from category.tf_models.classify_rcnn_model import ClassifyRcnnModel
-from category.tf_models import constant
+from rasa.nlu.classifiers.tf_models.classify_cnn_model import ClassifyCnnModel
+from rasa.nlu.classifiers.tf_models.classify_bilstm_model import ClassifyBilstmModel
+from rasa.nlu.classifiers.tf_models.classify_rcnn_model import ClassifyRcnnModel
+from rasa.nlu.classifiers.tf_models import constant
 import tensorflow as tf
 import os
 
